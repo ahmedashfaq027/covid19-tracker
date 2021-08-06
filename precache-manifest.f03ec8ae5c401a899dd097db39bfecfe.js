c2c0eb6372b75602abf0126c64338d92d39b80a3
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2021-08-06 18:50:24 +0530

    Updates

---
+++ precache-manifest.f03ec8ae5c401a899dd097db39bfecfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc28b34ea719f18ad77cd49de39c901c",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "7bda77066345d0cd99fc",
+    "url": "/covid19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "39fe0da5d2bfc2a68968",
+    "url": "/covid19-tracker/static/css/main.263c66c1.chunk.css"
+  },
+  {
+    "revision": "7bda77066345d0cd99fc",
+    "url": "/covid19-tracker/static/js/2.d013941d.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covid19-tracker/static/js/2.d013941d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fe0da5d2bfc2a68968",
+    "url": "/covid19-tracker/static/js/main.5c100862.chunk.js"
+  },
+  {
+    "revision": "d5823e235b668a0f5f4d",
+    "url": "/covid19-tracker/static/js/runtime-main.c99adbff.js"
+  }
+]);
